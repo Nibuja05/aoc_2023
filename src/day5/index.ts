785157ae189114b61e77ea6641c476beff274cee
AntonSVO <anton.winkler@schulverwalter.de>
2023-12-06 13:21:23 +0100

    better day 5/2 - still not right!

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -71,21 +71,37 @@ console.log(getTestInput()
 	.use(([seeds, ...other]) => <const>[
 		[...seeds.split(":")[1].matchAll(/\d+/g)]
 			.map(match => parseInt(match[0]))
-			.map((number, index, numbers) => index % 2 == 0? [number, numbers[index + 1]] : null)
-			.filter(numbers => numbers) as [number, number][]
+			.map((number, index, numbers) => index % 2 == 0? [number, number + numbers[index + 1] - 1] : null)
+			.filter(numbers => numbers) as number[][]
 		,...other.map((category) =>
 			category.split("\n").slice(1)
 			.map(line => [...line.matchAll(/\d+/g)]
 				.map(match => parseInt(match[0])) as [number, number, number]
 			)
+			.sort((a, b) => a[1] - b[1])
 		)
 	])
 	.use(([seeds, ...categories]) => 
-		categories.reduce((acc, category) => 
-			acc
+		categories.splice(0, 9).reduce((prev, category) => category
+			.reduce((acc, [_, src, len]) => acc.includes(src) ? [...acc, src + len] : [...acc, src, src + len], [0])
+			.reduce((acc, src) => 
+				acc.flatMap(([start, end]) => src > start && src < end
+					? [[start, src - 1], [src, end]]
+					: [[start, end]])
+			, prev)
+			.use(splitted => category
+				.reduce((acc, [dst, src, len]) => 
+					acc.map(([start, end]) => start >= src && end <= src + len
+						? [start + (dst - src), end + (dst - src)]
+						: [start, end]
+					)
+				, splitted)	
+			)
 		, seeds)
 	)
-	// .reduce((prev, cur) => Math.min(prev, cur))
+	// .filter(([i, _]) => i != 0)
+	// .reduce((prev, cur) => `${prev}\n${cur}`, "")
+	.reduce((prev, [cur, _]) => Math.min(prev, cur), Infinity)
 );
 
 // new idea: instead of seed numbers, take arrays with you:
